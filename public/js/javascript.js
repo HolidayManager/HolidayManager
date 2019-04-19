29127a7a5cc4b54519813989cce071cf70b11ed7
gcasoni@pt.lu <gcasoni@pt.lu>
2019-04-19 16:00:13 +0200

    pending holidays script (working)

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -34,26 +34,58 @@ $(function() {
 
       let id = $(this).attr("id");
 
-      let elementDiv = $(this).parent(".pendingRow");
+      let elementDiv = $(this).parent().parent();
 
       console.log(id);
       $.ajax(
           {
               url: '/holiday/accept/'+id,
               method: 'GET',
-              success: function(json,elementDiv){
-                  console.log(elementDiv);
+              success: function(json){
+                  console.log(this);
 
-                  $(elementDiv).html("Accepted");
-                  $(elementDiv).addClass(".accepted",{duration:500});
-                  $(elementDiv).fadeOut();
-              },
+                  $(this).html("Accepted");
+                  $(this).addClass("accepted");
+
+                  window.setTimeout(function(){
+                    $(this).fadeOut();
+                  }.bind(this), 3000);
+              }.bind(elementDiv),
               error: function(){
                   console.log("error");
               }
           }
       );
   });
+    $(".refuse").on("click",function(event){
+        event.preventDefault();
 
+
+
+        let id = $(this).parent().prev().children().attr("id");
+
+        let elementDiv = $(this).parent().parent();
+
+        console.log(id);
+        $.ajax(
+            {
+                url: '/holiday/refuse/'+id,
+                method: 'GET',
+                success: function(json){
+                    console.log(this);
+
+                    $(this).html("Refused");
+                    $(this).addClass("refused");
+
+                    window.setTimeout(function(){
+                        $(this).fadeOut();
+                    }.bind(this), 3000);
+                }.bind(elementDiv),
+                error: function(){
+                    console.log("error");
+                }
+            }
+        );
+    });
 
 });
